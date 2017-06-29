44bd0a6ebe635d4c83d7c293d55d945ff7f07f89
TheKing-Kodi <evansataz@hotmail.com>
2017-06-29 10:12:17 -0700

    update

--- toolz.service.macreader/default.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-import xbmc,xbmcgui,xbmcaddon
-
-while 1 :
-	macadress = xbmc.getInfoLabel('Network.MacAddress')	
-	if macadress != "Busy" and macadress != "" :
-		break
-xbmcgui.Dialog().ok("Mac Adress", "" + macadress.replace(":","-"))
